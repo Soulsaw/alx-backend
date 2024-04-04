049a3d150abb6615d7bd3f91a6289d855eb57a43
Soulsaw <ssawadogo292@gmail.com>
2024-04-04 13:43:41 +0000

    fix

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,17 +1,28 @@
-#!/usr/bin/env python3
-"""Doc module"""
-from base_caching import BaseCaching
-"""Import doc"""
+#!/usr/bin/python3
+''' Basic Dictionary: Create a class BasicCache that inherits from BaseCaching
+                      and is a caching system
+'''
+
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """Class doc"""
+    ''' A basic cache.
+        Inherits from class BaseCaching.
+        Attributes:
+          put - method that adds a key/value pair to cache
+          get - method that retrieves a key/value pair from cache
+    '''
+
     def put(self, key, item):
-        """ Add an item in the cache
-        """
-        if key is not None or item is not None:
+        ''' Add key/value pair to cache.
+        If either `key` or `item` is None, do nothing. '''
+        if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """Get methods doc"""
-        return self.cache_data.get(key, None)
+        ''' Return value stored in `key` of cache.
+        If key is None or does not exist in cache, return None. '''
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
