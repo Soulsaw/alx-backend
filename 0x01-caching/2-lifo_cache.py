9185c514ce0ca0b1758b922d6c3d43b49af47326
Soulsaw <ssawadogo292@gmail.com>
2024-04-04 14:04:24 +0000

    fix all

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python3
-"""Doc module"""
+'''
+This modele implement the caching replacement fifo
+'''
 from base_caching import BaseCaching
-"""Import class doc"""
+'''
+BaseCaching: inherit from the base_cache file
+'''
 
 
 class LIFOCache(BaseCaching):
-    """Class doc"""
+    '''This class inherit from the BaseCaching and have:
+    Args:
+    self.cache_data: from the parent constructor
+    methods:
+    put - To put a new item on the cache
+    get - To get an item with her key
+    '''
     def __init__(self):
-        """Doc init methods"""
+        '''
+        Implement the super methods from the parent
+        '''
         super().__init__()
 
     def put(self, key, item):
-        """ Add an item in the cache
-        """
+        '''
+        This function add an item in the cache
+        Args:
+        key - The key of the new item
+        item - The item to add
+        '''
         length = len(self.cache_data)
         lenitem = BaseCaching.MAX_ITEMS
         if key is not None or item is not None:
@@ -22,5 +38,7 @@ class LIFOCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
-        """Get methods doc"""
+        '''
+        This method return a giving key item
+        '''
         return self.cache_data.get(key, None)
