9af6ddad4067939044b304596d5746fd27eaf565
Soulsaw <ssawadogo292@gmail.com>
2024-04-04 13:53:48 +0000

    fix

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -14,7 +14,7 @@ class BasicCache(BaseCaching):
         '''
         add an item with his key
         '''
-        if key is not None or item is not None:
+        if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
