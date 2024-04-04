664709d89705fd91cdc70d1f3118bf3af3012fc3
Soulsaw <ssawadogo292@gmail.com>
2024-04-04 14:14:55 +0000

    fix

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,19 +1,35 @@
 #!/usr/bin/env python3
-"""Doc module"""
+'''
+This modele implement the caching replacement fifo
+'''
 from base_caching import BaseCaching
-"""Import class doc"""
+'''
+BaseCaching: inherit from the base_cache file
+'''
 
 
 class MRUCache(BaseCaching):
-    """Class doc"""
+    '''This class inherit from the BaseCaching and have:
+    Args:
+    self.cache_data: from the parent constructor
+    methods:
+    put - To put a new item on the cache
+    get - To get an item with her key
+    '''
     def __init__(self):
-        """Doc init methods"""
+        '''
+        Implement the super methods from the parent
+        '''
         super().__init__()
         self.keys = []
 
     def put(self, key, item):
-        """ Add an item in the cache
-        """
+        '''
+        This function add an item in the cache
+        Args:
+        key - The key of the new item
+        item - The item to add
+        '''
         length = len(self.cache_data)
         lenitem = BaseCaching.MAX_ITEMS
         if key is not None or item is not None:
@@ -33,5 +49,7 @@ class MRUCache(BaseCaching):
                 self.cache_data[key] = item
 
     def get(self, key):
-        """Get methods doc"""
+        '''
+        This method return a giving key item
+        '''
         return self.cache_data.get(key, None)
