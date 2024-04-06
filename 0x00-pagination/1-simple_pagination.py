e9bdb4d2f5e118963e9d5ceca7030da939ca2efb
Soulsaw <ssawadogo292@gmail.com>
2024-04-06 12:22:03 +0000

    fix task 1

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""Doc module"""
+import csv
+import math
+from typing import List
+import unittest
+
+
+def index_range(page, page_size):
+    """This function return a tuple of"""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
+
+
+class Server(unittest.TestCase):
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        '''The init method'''
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''
+        This function permit to return a giving range page
+        Args:
+        page - The started index page
+        page_size - The size of the page
+        Returns:
+        A lists of a != rows
+        '''
+        self.dataset()
+        pg, ps = index_range(page, page_size)
+        try:
+            return self.__dataset[pg:ps]
+        except Exception:
+            return []
