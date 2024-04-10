1f69c2e7b3d101ad4ad496fc65233d00d81107b6
Soulsaw <ssawadogo292@gmail.com>
2024-04-10 02:15:05 +0000

    fix

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Doc for a basic flask app
+"""
+from flask_babel import Babel, gettext as _
+from flask import Flask, render_template
+"""Import module doc"""
+app = Flask(__name__)
+
+
+class Config:
+    """Doc of the config class"""
+    LANGUES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """The root url for the home page"""
+    return render_template('3-index.html', title=_('home_title'),
+                           header=_('home_header'))
+
+
+if __name__ == "__main__":
+    """Definition of the main program"""
+    app.run()
