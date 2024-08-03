2dc9f1e05f49486d24057f3edee9b719f637a9a1
Soulsaw <ssawadogo292@gmail.com>
2024-08-03 01:01:37 +0000

    fix all

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -2,21 +2,17 @@
 """
 Doc for a basic flask app
 """
-from flask_babel import Babel, gettext, _
-from flask import Flask, render_template
+from flask_babel import Babel, _
+from flask import Flask, render_template, request
 """Import module doc"""
 app = Flask(__name__)
-
-
-class Config:
-    """Doc of the config class"""
-    LANGUES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
 babel = Babel(app)
-app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Define the babel local time"""
+    return request.accept_languages.best_match(["en", "fr"])
 
 
 @app.route("/", strict_slashes=False)
