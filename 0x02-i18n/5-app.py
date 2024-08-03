024302d1b1e2d0d728133a3a8fbed81756ffd6c0
Soulsaw <ssawadogo292@gmail.com>
2024-08-03 09:23:58 +0000

    fix task 5 and 6

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+Doc for a basic flask app
+"""
+from flask_babel import Babel
+from flask import Flask, render_template, request, g
+from typing import List, Dict
+"""Import module doc"""
+app = Flask(__name__)
+LANGUAGES = ['en', 'fr']
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_locale() -> List:
+    """Define the babel local time"""
+    user = getattr(g, 'user', None)
+    if user is not None:
+        locale = user.get('locale')
+        if locale in LANGUAGES:
+            return locale
+    return request.accept_languages.best_match(["en", "fr"])
+
+
+babel = Babel(app)
+babel.init_app(app, locale_selector=get_locale)
+
+
+def get_user(id) -> Dict:
+    """This function return the given user dictionnary"""
+    try:
+        return users.get(int(id), None)
+    except ValueError:
+        pass
+
+
+@app.before_request
+def before_request():
+    """Define the user before"""
+    id = request.args.get('login_as', None)
+    if id:
+        user = get_user(id)
+        g.user = user
+        if user is not None:
+            g.locale = g.user.get('locale')
+    else:
+        g.user = None
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """The root url for the home page"""
+    return render_template('5-index.html', user=g.user)
