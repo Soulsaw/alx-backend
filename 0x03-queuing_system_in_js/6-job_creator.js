1e2770f6c4479865616807590e64c28dc029e9d4
Soulsaw <ssawadogo292@gmail.com>
2024-08-27 14:06:46 +0000

    fix the task 7

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,16 +1,16 @@
 const kue = require('kue');
 const queue = kue.createQueue();
-const data = {
-    phoneNumber: String,
-    message: String
-}
-const job = queue.create('push_notification_code', data);
-job.save(()=>{
+
+const job = queue.create('push_notification_code', {
+    phoneNumber: "4153518780",
+    message: "This is the code to verify your account"
+});
+job.save(() => {
     console.log(`Notification job created: ${job.id}`);
 });
-job.on('complete', ()=>{
+job.on('complete', () => {
     console.log(`Notification job completed`);
 });
-job.on('failed', ()=>{
+job.on('failed', () => {
     console.log(`Notification job failed`);
 });
--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -4,4 +4,8 @@ const queue = kue.createQueue();
 function sendNotification(phoneNumber, message){
     console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
 }
-const job = queue.process('push_notification_code', sendNotification(4153518780, 'This is the code to verify your account'));
+queue.process('push_notification_code', (job, done)=>{
+    const {phoneNumber, message} = job.data;
+    sendNotification(phoneNumber, message);
+    done();
+});
