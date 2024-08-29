34ceacb48c511834a2fc3a603c6456cd6046ad05
Soulsaw <ssawadogo292@gmail.com>
2024-08-29 00:00:29 +0000

    fix the task 9

---
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -0,0 +1,37 @@
+import { expect } from "chai";
+import createPushNotificationsJobs from "./8-job";
+import { createQueue } from "kue";
+const queue = createQueue();
+
+before(function (done) {
+    queue.testMode.enter(true);
+    done();
+});
+
+afterEach(function () {
+    queue.testMode.clear();
+});
+
+after(function () {
+    queue.testMode.exit()
+})
+describe('createPushNotificationsJobs', () => {
+    it('tess', (done) => {
+        const list = [
+            {
+                phoneNumber: '4153518780',
+                message: 'This is the code 1234 to verify your account'
+            },
+            {
+                phoneNumber: '4153518786',
+                message: 'This is the code 12654 to verify your account'
+            }
+        ];
+        createPushNotificationsJobs(list, queue);
+        queue.testMode.jobs('push_notification_code_3', function (err, jobs) {
+            if (err) return done(err);
+            expect(jobs).to.have.lengthOf(list.length);
+            done();
+        });
+    })
+})
